389451fff5ddb07a9ca5d4eb574319d65832c921
nguyenquochung2357 <carzygaming2222@gmail.com>
2021-09-26 18:06:17 +0700

    update UI

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const path = require('path')
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 
